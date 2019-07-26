ec1fe5b9d9e891bb7eba4634e6a396a5e0865088
mhirano <Masahiro_Hirano@ipc.i.u-tokyo.ac.jp>
2019-07-26 15:15:23 +0900

    first commit

---
+++ include/Application.h
@@ -0,0 +1,48 @@
+//
+// Created by Masahiro Hirano <masahiro.dll@gmail.com>
+//
+
+#ifndef IMGUI_BAREBONE_APPLICATION_H
+#define IMGUI_BAREBONE_APPLICATION_H
+
+// dear imgui: standalone example application for SDL2 + OpenGL
+// If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
+// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan graphics context creation, etc.)
+// (GL3W is a helper library to access OpenGL functions since there is no standard header to access modern OpenGL functions easily. Alternatives are GLEW, Glad, etc.)
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <SDL.h>
+#include <string>
+
+// About OpenGL function loaders: modern OpenGL doesn't have a standard header file and requires individual function pointers to be loaded manually.
+// Helper libraries are often used for this purpose! Here we are supporting a few common ones: gl3w, glew, glad.
+// You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>    // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>    // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>  // Initialize with gladLoadGL()
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+class Application {
+    SDL_Window* window;
+    SDL_GLContext gl_context;
+    std::string glsl_version;
+
+public:
+    Application();
+    ~Application();
+
+    bool run();
+
+};
+
+
+
+#endif //IMGUI_BAREBONE_APPLICATION_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "Application.h"
+
+int main(int, char**)
+{
+  Application app;
+
+  return app.run();
+}
