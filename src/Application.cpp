0e243af30ee3921e42bdb87dfd6d7511ec0467b7
mhirano <Masahiro_Hirano@ipc.i.u-tokyo.ac.jp>
2019-08-07 17:36:16 +0900

    make it run on flash

--- src/Application.cpp
+++ src/Application.cpp
@@ -24,7 +24,7 @@ Application::Application() {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
     // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    glsl_version = "#version 130";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
